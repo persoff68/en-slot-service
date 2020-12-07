c5d7b22d278f067ae290346d08de0ba6695a7757
Vladimir Perfilev <vladimir.perfilev@sidion.de>
2020-12-07 20:13:12 +0100

    init

---
+++ src/test/java/com/escapenavigator/slot/EnSlotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escapenavigator.slot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnSlotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
